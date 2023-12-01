ebde005520237ed814e1779b57ad37727d36dcde
BrightDN <brightondeneef@gmail.com>
2023-12-01 19:24:20 +0100

    Finished puzzle day1
    The full solution to the puzzle

--- day1/solution.js
+++ day1/solution.js
@@ -1,16 +1,54 @@
-const submitBtn = document.getElementsByTagName("button")[0]
-submitBtn.addEventListener("click", solvePuzzle)
+const submitBtn = document.getElementsByTagName("button")
+submitBtn[0].addEventListener("click", function(){solvePuzzle(1)})
+submitBtn[1].addEventListener("click", function(){solvePuzzle(2)})
 
-function solvePuzzle(){
-  const textInput = document.getElementsByTagName("textarea")[0].value
-  
-  showSolution(transformData(textInput))
+function solvePuzzle(part){
+  const textInput = document.getElementsByTagName("textarea")[0].value 
+  showSolution(transformData(textInput, part))
 }
 
-function transformData(dataToTransform){
-  return 
+function transformData(dataToTransform, part){
+  if(part == 2){
+const workableData = dataToTransform
+.replace(/(oneight)/gi, "oneeight")
+.replace(/(twone)/gi, "twoone")
+.replace(/(threeight)/gi, "threeeight")
+.replace(/(fiveight)/gi, "fiveeight")
+.replace(/(sevenine)/gi, "sevennine")
+.replace(/(eightwo)/gi, "eighttwo")
+.replace(/(eighthree)/gi, "eightthree")
+.replace(/(nineight)/gi, "nineeight")
+.replace(/(one)/gi, 1)
+.replace(/(two)/gi, 2)
+.replace(/(three)/gi, 3)
+.replace(/(four)/gi, 4)
+.replace(/(five)/gi, 5)
+.replace(/(six)/gi, 6)
+.replace(/(seven)/gi, 7)
+.replace(/(eight)/gi, 8)
+.replace(/(nine)/gi, 9)
+
+dataToTransform = workableData
+  }
+  const newArray = []
+  let summedUp = 0;
+dataToTransform
+  .replace(/([a-zA-Z])/g, "")
+  .split("\n")
+  .forEach(element => newArray.push(`${element.charAt(0)}${element.charAt((element.length-1))}`))
+  
+  newArray.forEach(element => summedUp += parseInt(element))
+  return summedUp
 }
 
 function showSolution(dataToShow){
-  
+  createResult(dataToShow)
+}
+
+function createResult(dataToShow){
+  const createParagraph = document.createElement("p");
+  createParagraph.innerHTML = `The answer, based on the given input, is: ${dataToShow}`
+  createParagraph.setAttribute("name", "answer");
+  if(document.querySelectorAll("[name=answer]").length > 0)document.querySelector("body").removeChild(document.querySelector("[name=answer]"));
+  document.querySelector("body").appendChild(createParagraph);
 }
